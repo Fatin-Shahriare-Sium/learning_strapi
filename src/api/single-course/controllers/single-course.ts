8eef3d91d45e9d87b894adb288c302fa18a1e4a5
unknown <sium1206@gmail.com>
2024-12-16 15:36:06 +0600

    alhamdulillah,add enroll course controller

--- src/api/single-course/controllers/single-course.ts
+++ src/api/single-course/controllers/single-course.ts
@@ -2,6 +2,17 @@
  * single-course controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::single-course.single-course');
+export default factories.createCoreController("api::single-course.single-course", ({ strapi }) => ({
+  async enrolledCourseforUser(ctx) {
+    const { courseId, userDocId } = ctx.request.query;
+    let courseIdx = String(courseId);
+    let userDocIdx = String(userDocId);
+    await strapi.documents("plugin::users-permissions.user").update({
+      documentId: userDocIdx,
+      data: { enrolledCourses: [courseIdx] },
+    });
+    return { data: ctx.request.query };
+  },
+}));
---
+++ src/api/single-course/routes/customRoute.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/single-courses/enroll-course",
+      handler: "single-course.enrolledCourseforUser",
+    },
+  ],
+};
